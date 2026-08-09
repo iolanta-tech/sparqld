d9d405119e317a1688827bc99b9ac59fee5a79e3
Anatoly Scherbakov <altaisoft@gmail.com>
2026-08-09 23:13:59 +0400

    Add stub `lint` jeeves command

--- jeeves.py
+++ jeeves.py
@@ -13,3 +13,9 @@ docs = typer.Typer(no_args_is_help=True)
 def _serve():
     """Serve the documentation with MkDocs."""
     sh.Command(sys.executable)('-m', 'mkdocs', 'serve', _fg=True)
+
+
+def lint():
+    """Lint the project."""
+    # TODO: cargo fmt & lint
+    # TODO: in parallel, python fmt & lint
